39a74de75d1b0d3381562f1a96515806b7de7fe2
joluoch <jakindaodhiambo@gmail.com>
2021-11-05 15:26:02 +0300

    merging two list

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -354,6 +354,57 @@ class MyLinkedList:
         left=left.next
         
         return isp1
+    
+
+    def mergeTwoLists(self, l1,l2):
+
+        if l1 == None:
+            return l1
+        if l2 == None:
+            return l2
+        
+        output = None
+        
+        if l1.val<l2.val:
+            output = l1
+            l1.next = self.mergeTwoLists(l1.next,l2)
+        else:
+            output = l2
+            l2.next = self.mergeTwoLists(l1,l2.next)
+            
+        return output
+    '''def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+        
+        output = None
+        
+        if l1.val<l2.val:
+            output = l1
+            l1 = l1.next
+        else:
+            output = l2
+            l2= l2.next
+        curr = output
+        
+        while l1 != None and l2 != None:
+            if l1.val > l2.val:
+                curr.next = l2
+                l2 = l2.next
+            else:
+                curr.next = l1
+                l1 = l1.next
+            curr = curr.next
+        if l1 != None:
+            curr.next = l1
+        if l2 !=None:
+            curr.next = l2
+            
+        return output
+        '''
+                
 
         
         
