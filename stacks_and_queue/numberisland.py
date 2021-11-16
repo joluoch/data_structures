914cd14d2050422c634baaef781292d5bee1b21e
joluoch <jakindaodhiambo@gmail.com>
2021-11-16 21:43:34 +0300

    number of islands

--- stacks_and_queue/numberisland.py
+++ stacks_and_queue/numberisland.py
@@ -1,7 +1,7 @@
 #bfs approach 
 import collections
 class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
+    def numIslands(self, grid) -> int:
         if not grid:
             return 0
         rows,cols = len(grid),len(grid[0])
