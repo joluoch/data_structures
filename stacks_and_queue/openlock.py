4361ba08c75c682e46b2c5177857391fbc5fa2cd
joluoch <jakindaodhiambo@gmail.com>
2021-11-16 21:31:32 +0300

    queues and stack leetcode questions

---
+++ stacks_and_queue/openlock.py
@@ -0,0 +1,25 @@
+import collections
+
+def openLock(self, deadends: List[str], target: str) -> int:
+        nums = []
+        deadends = set(deadends)
+        
+        if '0000' in deadends: return -1
+        
+        #bfs
+        
+        q = collections.deque([('0000',0)])
+        
+        visited = set()
+        
+        while q :
+            cand,steps = q.popleft()
+            if cand == target: return steps
+            
+            for i in range (4):
+                for digit in [((int(cand[i])+1)%10),((int(cand[i])-1)%10)]:#moving up or down
+                    nxt = cand[:i] + str(digit) + cand[i+1:]
+                    if nxt not in deadends and nxt not in visited:
+                        visited.add(nxt)
+                        q.append((nxt,steps+1))
+        return -1
