2712f25c0cee580f066bb8aaac42a0390d0bba61
joluoch <jakindaodhiambo@gmail.com>
2021-11-30 23:26:12 +0300

    few changes

--- ArrayString/arrays.py
+++ ArrayString/arrays.py
@@ -424,7 +424,7 @@ def arrayPairSum(self, nums: List[int]) -> int:
         return sum
 '''
 ''' 
-    TWO SUM I
+    TWO SUM I #micro
     Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
     You may assume that each input would have exactly one solution, and you may not use the same element twice.
